f361f441429ca120c7d81fae84c9e2f7fd9fcabf
Pavithran Ramachandran <pavithranrao.1512@gmail.com>
2021-06-01 08:51:32 -0700

    versioning source code using cmake

--- main.cpp
+++ main.cpp
@@ -4,27 +4,29 @@
 #include <iostream>
 #include <adder.h>
 #include <GLFW/glfw3.h>
+#include <cmake_demo.h>
 
 
-int main() {
+int main(int argc, char* argv[]) {
     std::cout << "Hello, World!" << std::endl;
     std::cout << add(10.5, 20.5) << std::endl;
+    std::cout << PROJECT_VER_MAJOR << "." << PROJECT_VER_MINOR  << std::endl;
 
-    GLFWwindow* window;
-    int width, height;
-
-    if( !glfwInit() )
-    {
-        fprintf( stderr, "Failed to initialize GLFW\n" );
-        exit( EXIT_FAILURE );
-    }
-
-    glfwWindowHint(GLFW_DEPTH_BITS, 16);
-    glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
-
-    window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
-
-    glfwDestroyWindow(window);
+//    GLFWwindow* window;
+//    int width, height;
+//
+//    if( !glfwInit() )
+//    {
+//        fprintf( stderr, "Failed to initialize GLFW\n" );
+//        exit( EXIT_FAILURE );
+//    }
+//
+//    glfwWindowHint(GLFW_DEPTH_BITS, 16);
+//    glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
+//
+//    window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
+//
+//    glfwDestroyWindow(window);
 
     return 0;
 }
