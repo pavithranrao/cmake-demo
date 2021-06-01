7409f220999808878f6b4f5b6561ae240fbb74c8
Pavithran Ramachandran <pavithranrao.1512@gmail.com>
2021-06-01 09:29:55 -0700

    optional lib

--- main.cpp
+++ main.cpp
@@ -2,14 +2,19 @@
 // https://www.youtube.com/watch?v=nlKcXPUJGwA&list=PLalVdRk2RC6o5GHu618ARWh0VO0bFlif4&index=1
 
 #include <iostream>
-#include <adder.h>
 #include <GLFW/glfw3.h>
 #include <cmake_demo.h>
 
 
 int main(int argc, char* argv[]) {
     std::cout << "Hello, World!" << std::endl;
+#ifdef USE_ADDER
+    std::cout << "using adder" << std::endl;
     std::cout << add(10.5, 20.5) << std::endl;
+#else
+    std::cout << "not using adder" << std::endl;
+    std::cout << 10.5 + 20.5 << std::endl;
+#endif
     std::cout << PROJECT_NAME << " v" << PROJECT_VER_MAJOR << "." << PROJECT_VER_MINOR  << std::endl;
 
 //    GLFWwindow* window;
